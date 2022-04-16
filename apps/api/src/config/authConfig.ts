ea399a28e8f80d2a2bf94925ddfad996c83b5edd
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2022-04-16 20:14:54 +0530

    refactor: removed unwanted arrow fn body

--- apps/api/src/config/authConfig.ts
+++ apps/api/src/config/authConfig.ts
@@ -1,20 +1,16 @@
 import { registerAs } from '@nestjs/config';
 
-export const authConfig = registerAs('auth', () => {
-  const configurations = {
-    jwt: {
-      secret: process.env.JWT_SECRET,
-      issuer: process.env.JWT_ISSUER,
-      validity: process.env.JWT_VALIDITY,
-    },
+export const authConfig = registerAs('auth', () => ({
+  jwt: {
+    secret: process.env.JWT_SECRET,
+    issuer: process.env.JWT_ISSUER,
+    validity: process.env.JWT_VALIDITY,
+  },
 
-    google: {
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    },
-  };
-
-  return configurations;
-});
+  google: {
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  },
+}));
 
 export type AuthConfig = ReturnType<typeof authConfig>;
