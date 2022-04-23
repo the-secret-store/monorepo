e812fd749c8fd067ef7a3420fa7d321286d0aa01
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2022-04-23 17:13:20 +0530

    fix(util): hex string generator returning null

--- apps/api/src/utils/generate-hex-string.ts
+++ apps/api/src/utils/generate-hex-string.ts
@@ -1,9 +1,5 @@
 import { randomBytes } from 'crypto';
 
 export function generateHexString(size: number): string {
-  let str: string;
-  randomBytes(size, (_err, buffer) => {
-    str = buffer.toString('hex');
-  });
-  return str;
+  return randomBytes(size).toString('hex');
 }
