4dfd6fff355b84a8731e7cfaa41e8d7d91f6b40c
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2022-05-01 22:41:08 +0530

    fix: get-secrets error due to auth-tag

--- apps/api/src/utils/EncryptionService.ts
+++ apps/api/src/utils/EncryptionService.ts
@@ -1,16 +1,7 @@
-import {
-  CipherCCMTypes,
-  CipherGCMTypes,
-  createCipheriv,
-  createDecipheriv,
-  randomBytes,
-} from 'crypto';
+import { CipherGCMTypes, createCipheriv, createDecipheriv, randomBytes } from 'crypto';
 
 export class EncryptionService {
-  constructor(
-    private readonly key: string,
-    private readonly algorithm: CipherCCMTypes | CipherGCMTypes
-  ) {}
+  constructor(private readonly key: string, private readonly algorithm: CipherGCMTypes) {}
 
   encryptValues(keyPlainTextPair: Record<string, string>) {
     if (!keyPlainTextPair) return {};
@@ -37,16 +28,20 @@ export class EncryptionService {
     const cipher = createCipheriv(this.algorithm, Buffer.from(this.key, 'hex'), iv);
 
     const encrypted = Buffer.concat([cipher.update(plainText), cipher.final()]);
-    return `${iv.toString('hex')}.${encrypted.toString('hex')}`;
+    return `${iv.toString('hex')}.${encrypted.toString('hex')}.${cipher
+      .getAuthTag()
+      .toString('hex')}`;
   }
 
   decrypt(cipherText: string) {
-    const [iv, encrypted] = cipherText.split('.');
+    const [iv, encrypted, authTag] = cipherText.split('.');
     const decipher = createDecipheriv(
       this.algorithm,
       Buffer.from(this.key, 'hex'),
       Buffer.from(iv, 'hex')
     );
+
+    decipher.setAuthTag(Buffer.from(authTag, 'hex'));
 
     return Buffer.concat([
       decipher.update(Buffer.from(encrypted, 'hex')),
