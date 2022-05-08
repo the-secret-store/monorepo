974259e321f7d574405458e0c3a2fc0597af3eed
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2022-05-08 20:02:03 +0530

    fix: match button styles to text-input

--- apps/web/src/lib/components/Button/button.style.tsx
+++ apps/web/src/lib/components/Button/button.style.tsx
@@ -3,19 +3,18 @@ import styled from 'styled-components';
 export const ButtonStyleWrapper = styled.button<{ variant: ButtonVariants }>`
   display: inline-flex;
   font-family: inherit;
-  font-size: 1.8rem;
   min-width: 12rem;
   padding: 1rem 2rem;
   align-items: center;
   justify-content: center;
-  border-radius: ${props => (props.variant === 'error' ? '3rem' : '0.5rem')};
+  border-radius: ${props => (props.variant === 'error' ? '3rem' : '0.2rem')};
   text-transform: uppercase;
   cursor: pointer;
   transition: all 0.2s ease-in-out;
   border: none;
   outline: none;
   background-color: ${props => props.theme.colors[props.variant === 'primary' ? 'purple' : 'red']};
-  color: ${props => props.theme.colors.white};
+  color: ${({ theme, variant }) => (variant === 'error' ? theme.colors.white : theme.colors.black)};
 
   &:hover {
     box-shadow: 0 0 1rem
