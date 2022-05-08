e14a9b26ea7ddc98f29c01a8d9aa532db95f3a79
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2022-05-08 19:55:57 +0530

    refactor: added index exports for images

---
+++ apps/web/src/assets/index.ts
@@ -0,0 +1,2 @@
+export { default as LogoPng } from './logo.png';
+export { default as LogoSvg } from './logo.svg';
--- apps/web/src/views/Landing/index.tsx
+++ apps/web/src/views/Landing/index.tsx
@@ -1,10 +1,10 @@
 import { LandingPageStyles } from './landing.style';
-import Logo from '../../assets/Logo.png';
+import { LogoPng } from '../../assets';
 
 export function LandingPage() {
   return (
     <LandingPageStyles>
-      <img src={Logo} alt="Logo" />
+      <img src={LogoPng} alt="Logo" />
       <h1>The Secret Store</h1>
     </LandingPageStyles>
   );
