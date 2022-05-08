0073134842cbbe76a9d0d00f175740760581f565
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2022-05-08 14:30:49 +0530

    stash: removed web app

--- apps/web/src/app/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './ThemeEngine';
-export * from './Router';
-export * from './Shell';
--- apps/web/src/views/home/home.view.tsx
+++
@@ -1,5 +0,0 @@
-import { Title } from '@mantine/core';
-
-export function Home() {
-  return <Title order={1}>Hi</Title>;
-}
