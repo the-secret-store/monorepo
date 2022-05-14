5242f173f3ad2a9f864e426443728e7f49391e9b
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2022-05-14 11:21:07 +0530

    style: improved chip style

--- apps/web/src/lib/components/Chip/chip.style.tsx
+++ apps/web/src/lib/components/Chip/chip.style.tsx
@@ -4,9 +4,10 @@ import styled from 'styled-components';
 export const ChipStyleWrapper = styled.span<{ color?: keyof ITheme['colors'] }>`
   display: flex;
   align-items: center;
-  font-size: 1rem;
+  font-size: 1.2rem;
+  gap: 0.3rem;
   padding: 0.5rem 1rem;
-  border-radius: 1rem;
+  border-radius: 1.4rem;
   background-color: ${({ theme, color }) => theme.colors[color ?? 'purple']};
   color: ${({ theme }) => theme.colors.black};
   min-width: 3rem;
