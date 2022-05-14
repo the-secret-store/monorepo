512511ed8fb9108bdf64fc719df8b5268c3039e8
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2022-05-15 00:11:52 +0530

    feat: added support for external links in button component

--- apps/web/src/lib/components/Button/index.tsx
+++ apps/web/src/lib/components/Button/index.tsx
@@ -1,10 +1,11 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import { useExternalLink, useHyperLink } from '$web/hooks';
 import { ButtonHTMLAttributes } from 'react';
-import { useHyperLink } from '$web/hooks';
 import { ButtonStyleWrapper, ButtonVariants } from './button.style';
 
-export function Button({ children, variant, link, onClick, ...rest }: IButtonProps) {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
+export function Button({ children, variant, link, href, onClick, ...rest }: IButtonProps) {
   if (link) onClick = useHyperLink(link);
+  if (href) onClick = useExternalLink(href);
 
   return (
     <ButtonStyleWrapper variant={variant ?? 'primary'} onClick={onClick} {...rest}>
@@ -16,4 +17,5 @@ export function Button({ children, variant, link, onClick, ...rest }: IButtonPro
 export interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: ButtonVariants;
   link?: string;
+  href?: string;
 }
