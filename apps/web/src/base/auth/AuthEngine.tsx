e6a7fc7b55b5faaaf0d4beae7ab391cd43eb0e9f
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2022-05-15 00:27:46 +0530

    refactor: renamed auth flag in AuthEngine

--- apps/web/src/base/auth/AuthEngine.tsx
+++ apps/web/src/base/auth/AuthEngine.tsx
@@ -3,13 +3,13 @@ import { SessionManager } from './SessionManager';
 
 export const AuthContext = createContext<AuthContextType>({
   session: new SessionManager(),
-  auth: false,
+  isAuthenticated: false,
   setAuthToken: old => old,
 });
 
 type AuthContextType = {
   session: SessionManager;
-  auth: boolean;
+  isAuthenticated: boolean;
   setAuthToken: (token: string) => void;
 };
 
@@ -19,19 +19,19 @@ export const useAuthApi = () => useContext(AuthContext);
 
 export function AuthEngine({ children }: AuthEngineProps) {
   const session = new SessionManager();
-  const [auth, setAuth] = useState(() => session.isAuthenticated());
+  const [isAuthenticated, setAuthenticationFlag] = useState(() => session.isAuthenticated());
 
   function setAuthToken(token: string) {
     if (token.length === 0) {
       session.clearSession();
-      setAuth(false);
+      setAuthenticationFlag(false);
     } else {
       session.setSession(token);
-      setAuth(true);
+      setAuthenticationFlag(true);
     }
   }
 
-  return <AuthProvider value={{ session, auth, setAuthToken }}>{children}</AuthProvider>;
+  return <AuthProvider value={{ session, isAuthenticated, setAuthToken }}>{children}</AuthProvider>;
 }
 
 interface AuthEngineProps {
