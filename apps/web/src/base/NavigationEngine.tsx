6524af1b624ac0143434d367947c2c577fb8851a
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2022-05-15 00:36:53 +0530

    feat: integrated route handlers

--- apps/web/src/base/NavigationEngine.tsx
+++ apps/web/src/base/NavigationEngine.tsx
@@ -11,25 +11,32 @@ import { TestPage } from '$web/views/Test';
 import { UserSettings } from '$web/views/user';
 import { Outlet, Route, Routes } from 'react-router-dom';
 import { Error404 } from './error/404';
+import { AuthRoute, ProtectedRoute } from './routes';
 
 export function NavigationEngine() {
   return (
     <Routes>
-      <Route index element={<LandingPage />} />
-      <Route path="/auth/login" element={<Login />} />
-      <Route path="/test" element={<TestPage />} />
-      <Route path="/projects" element={<Outlet />}>
-        <Route index element={<Projects />} />
-        <Route path="create" element={<CreateProject />} />
-        <Route path=":projectId" element={<Outlet />}>
-          <Route index element={<ProjectOverview />} />
-          <Route path="invite" element={<InviteUserPage />} />
-          <Route path="show-secrets-as-json" element={<ShowSecretsAsJson />} />
-        </Route>
+      <Route element={<AuthRoute redirect="/projects" />}>
+        <Route path="/auth/login" element={<Login />} />
       </Route>
 
-      <Route path="/user" element={<Outlet />}>
-        <Route path="settings" element={<UserSettings />} />
+      <Route index element={<LandingPage />} />
+
+      <Route element={<ProtectedRoute />}>
+        <Route path="/test" element={<TestPage />} />
+        <Route path="/projects" element={<Outlet />}>
+          <Route index element={<Projects />} />
+          <Route path="create" element={<CreateProject />} />
+          <Route path=":projectId" element={<Outlet />}>
+            <Route index element={<ProjectOverview />} />
+            <Route path="invite" element={<InviteUserPage />} />
+            <Route path="show-secrets-as-json" element={<ShowSecretsAsJson />} />
+          </Route>
+        </Route>
+
+        <Route path="/user" element={<Outlet />}>
+          <Route path="settings" element={<UserSettings />} />
+        </Route>
       </Route>
 
       <Route path="*" element={<Error404 />} />
