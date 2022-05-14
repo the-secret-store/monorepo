63fecdce3ad8d8f470b97f07a647c1b36230d449
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2022-05-15 00:06:23 +0530

    feat: setup page redirect without history in frontend

--- apps/web/src/views/auth/Login/index.tsx
+++ apps/web/src/views/auth/Login/index.tsx
@@ -1,11 +1,17 @@
 import { LogoPng } from '$web/assets/images';
 import { Button } from '$web/components/Button';
-import { useSearchParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import { LoginStyleWrapper } from './login.style';
 
 export function Login() {
   const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
   const token = searchParams.get('token');
+
+  useEffect(() => {
+    if (token) navigate('/projects', { replace: true });
+  }, [token, navigate]);
 
   return (
     <LoginStyleWrapper>
