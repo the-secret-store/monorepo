a416256a93ff3d8b9f0ab5234f7eccb6e55da9b3
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2022-05-15 08:59:26 +0530

    fix: enabled cors for client url

--- apps/api/src/setup/setup-app.ts
+++ apps/api/src/setup/setup-app.ts
@@ -1,12 +1,19 @@
-import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { INestApplication, Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { RootConfig } from '../config';
+import picocolors = require('picocolors');
+import { MiscConfig, RootConfig } from '../config';
 
 export function setupApp(app: INestApplication, configService: ConfigService) {
   const config = configService.get<RootConfig>('root');
+  const misc = configService.get<MiscConfig>('misc');
   const globalPrefix = config.prefix;
 
   app.setGlobalPrefix(globalPrefix);
+
+  app.get(Logger).debug(`Enabling CORS for ${picocolors.cyan(misc.clientUrl)}`, 'ApplicationRoot');
+  app.enableCors({
+    origin: misc.clientUrl,
+  });
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 }
