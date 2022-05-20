b0104e7aa0cc60927dd450fdf2fc8efa9eef5a58
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2022-05-20 07:16:40 +0530

    fix: app crash due to missing mandatory props for 404 page

--- apps/web/src/base/error/404/Error404.tsx
+++ apps/web/src/base/error/404/Error404.tsx
@@ -2,9 +2,9 @@ import { Globe } from '@styled-icons/boxicons-regular/Globe';
 import { Button } from '$web/components';
 import { Error404StyleWrapper } from './404.style';
 
-export function Error404({ container }: { container: 'shell' | 'none' } = { container: 'none' }) {
+export function Error404({ container }: { container?: 'shell' | 'none' }) {
   return (
-    <Error404StyleWrapper container={container}>
+    <Error404StyleWrapper container={container ?? 'none'}>
       <div>
         4<Globe size={80} />4
       </div>
