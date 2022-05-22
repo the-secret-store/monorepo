5102a9e301989fb8d3c679564291424f5f0a60d5
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2022-05-22 17:00:54 +0530

    refactor: switched to LoggerService instead of console log

--- apps/cli/src/lib/util/BrowserService.ts
+++ apps/cli/src/lib/util/BrowserService.ts
@@ -1,13 +1,15 @@
 import * as openInBrowser from 'open';
 import { Injectable } from '@nestjs/common';
+import { CliLoggerService } from './CliLoggerService';
 
 @Injectable()
 export class BrowserService {
   private baseUrl = 'http://localhost:4200';
+  private loggerService = new CliLoggerService(BrowserService.name);
 
   open(route: string) {
     if (!route.startsWith('/')) route = `/${route}`;
-    console.debug('Attempting to', `${this.baseUrl}${route}`, 'on browser');
+    this.loggerService.debug(`Attempting to ${this.baseUrl}${route} on browser`);
     openInBrowser(`${this.baseUrl}${route}`);
   }
 }
