e73c84a7dca060efedcd4f7dd9c7a4a5cb3f2e62
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2022-05-22 17:40:07 +0530

    feat: added login command with inquirer

--- apps/cli/src/modules/auth/auth.module.ts
+++ apps/cli/src/modules/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { BrowserService } from '../../lib/util';
 import { Login } from './login/login.command';
+import { LoginQuestions } from './login/login.questions';
 
 @Module({
   imports: [],
-  providers: [Login],
+  providers: [Login, BrowserService, LoginQuestions],
   exports: [Login],
 })
 export class AuthModule {}
--- apps/cli/src/modules/auth/login/login.command.ts
+++ apps/cli/src/modules/auth/login/login.command.ts
@@ -1,11 +1,31 @@
-import { Command, CommandRunner } from 'nest-commander';
+import { BrowserService, CliLoggerService } from '../../../lib/util';
+import { Command, CommandRunner, InquirerService, Option } from 'nest-commander';
 
 @Command({
   name: 'login',
-  options: { isDefault: false },
+  options: { isDefault: true },
 })
 export class Login implements CommandRunner {
-  async run(): Promise<void> {
-    console.log('Executing login');
+  private readonly loggerService = new CliLoggerService('Login');
+  constructor(private browserService: BrowserService, private readonly inquirer: InquirerService) {}
+
+  async run(_inputs: string[], options: Record<string, string>): Promise<void> {
+    let { token } = options;
+
+    if (!token) {
+      this.loggerService.info('Generate a new auth token and paste it here');
+      this.browserService.open('user/settings');
+      token = (await this.inquirer.ask<{ token: string }>('token', undefined)).token;
+    }
+
+    this.loggerService.debug(token, 'Received');
+  }
+
+  @Option({
+    flags: '-t, --token <token>',
+    description: 'Json Web Token obtained from the web client',
+  })
+  parseShell(val: string) {
+    return val;
   }
 }
