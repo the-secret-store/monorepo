6edf21f16e6853d1cfdc145fe19dd58d53030663
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2022-05-22 16:39:34 +0530

    fix: made BrowserService injectible

--- apps/cli/src/lib/util/BrowserService.ts
+++ apps/cli/src/lib/util/BrowserService.ts
@@ -1,10 +1,13 @@
 import * as openInBrowser from 'open';
+import { Injectable } from '@nestjs/common';
 
+@Injectable()
 export class BrowserService {
-  private baseUrl: 'http://localhost:4200';
+  private baseUrl = 'http://localhost:4200';
 
   open(route: string) {
     if (!route.startsWith('/')) route = `/${route}`;
+    console.debug('Attempting to', `${this.baseUrl}${route}`, 'on browser');
     openInBrowser(`${this.baseUrl}${route}`);
   }
 }
