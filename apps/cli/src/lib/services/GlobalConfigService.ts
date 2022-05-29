9ba6c4c11e93bdd5ee809efa63833a5f89b45c65
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2022-05-29 21:41:01 +0530

    feat: added typing to GlobalConfigService

--- apps/cli/src/lib/services/GlobalConfigService.ts
+++ apps/cli/src/lib/services/GlobalConfigService.ts
@@ -5,17 +5,20 @@ import { CliLoggerService } from './CliLoggerService';
 
 export class GlobalConfigService {
   private readonly CONFIG_FILE_PATH = path.join(os.homedir(), '.tssrc');
-  private config: Record<string, string> = {};
+  private config: Configurations = {
+    noOfLocalBackups: 1,
+    preferredEditor: 'code',
+  };
 
   constructor(private readonly logger: CliLoggerService = new CliLoggerService()) {
-    this.config = JsonHelper.importJsonSync(this.CONFIG_FILE_PATH, true) as Record<string, string>;
+    this.config = JsonHelper.importJsonSync(this.CONFIG_FILE_PATH, true) as Configurations;
   }
 
-  get(key: string) {
+  get(key: keyof Configurations) {
     return this.config[key];
   }
 
-  set(key: string, value: string) {
+  set(key: keyof Configurations, value: string | number) {
     this.config[key] = value;
     this.logger.debug(`Attempting to write to ${this.CONFIG_FILE_PATH}`);
     return JsonHelper.writeJson(this.CONFIG_FILE_PATH, this.config);
@@ -28,4 +31,10 @@ export class GlobalConfigService {
   setAccessToken(token: string) {
     return this.set('token', token);
   }
+}
+
+export interface Configurations extends Record<string, unknown> {
+  token?: string;
+  noOfLocalBackups: number;
+  preferredEditor: string;
 }
