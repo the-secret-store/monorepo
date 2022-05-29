675584bf17df8faae71805101d47381d28871eb5
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2022-05-29 20:47:29 +0530

    feat: added JsonHelper utility class

--- apps/cli/src/lib/services/GlobalConfigService.ts
+++ apps/cli/src/lib/services/GlobalConfigService.ts
@@ -1,24 +1,14 @@
-import * as path from 'path';
 import * as os from 'os';
-import * as fsp from 'fs/promises';
-import * as fs from 'fs';
+import * as path from 'path';
+import { JsonHelper } from '$cli/util';
 import { CliLoggerService } from './CliLoggerService';
 
 export class GlobalConfigService {
   private readonly CONFIG_FILE_PATH = path.join(os.homedir(), '.tssrc');
   private config: Record<string, string> = {};
 
-  private async loadConfig() {
-    const fileContent = await fsp.readFile(this.CONFIG_FILE_PATH, 'utf8');
-    this.config = JSON.parse(fileContent);
-  }
-
   constructor(private readonly logger: CliLoggerService = new CliLoggerService()) {
-    if (!fs.existsSync(this.CONFIG_FILE_PATH)) {
-      logger.debug('Config file not found, creating an empty config file...');
-      fs.writeFileSync(this.CONFIG_FILE_PATH, '{}');
-    }
-    this.loadConfig();
+    this.config = JsonHelper.importJsonSync(this.CONFIG_FILE_PATH, true) as Record<string, string>;
   }
 
   get(key: string) {
@@ -28,7 +18,7 @@ export class GlobalConfigService {
   set(key: string, value: string) {
     this.config[key] = value;
     this.logger.debug(`Attempting to write to ${this.CONFIG_FILE_PATH}`);
-    return fsp.writeFile(this.CONFIG_FILE_PATH, JSON.stringify(this.config));
+    return JsonHelper.writeJson(this.CONFIG_FILE_PATH, this.config);
   }
 
   getAccessToken() {
