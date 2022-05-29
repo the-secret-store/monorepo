2b349ff5b1cf836f2f7a9e8d1dafbc5d7e3a988b
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2022-05-29 18:50:17 +0530

    feat: enabled nest-logger in dev mode using CLI_DEV_FLAG

--- apps/cli/src/main.ts
+++ apps/cli/src/main.ts
@@ -1,8 +1,12 @@
+import { Logger } from '@nestjs/common';
 import { CommandFactory } from 'nest-commander';
 import { AppModule } from './modules/app/app.module';
 
 const bootstrap = async () => {
-  await CommandFactory.run(AppModule);
+  await CommandFactory.run(
+    AppModule,
+    !+process.env.CLI_DEV_FLAG ? { logger: false } : new Logger()
+  );
 };
 
 bootstrap();
